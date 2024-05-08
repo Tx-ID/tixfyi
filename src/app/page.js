12342289c1d79d2150dc55f2a65a64fd7bbdccf0
Tix <67917789+Tx-ID@users.noreply.github.com>
2024-05-08 23:04:21 +0700

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,6 @@ import { OsuIcon } from "@/components/osu.js"
 import { useEffect, useState } from "react"
 import Image from 'next/image'
 
-const video_url = "https://cdn.discordapp.com/attachments/694736624071737376/1159494506538864701/kokonaenae.mp4"
-
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   const [alertText, setAlert] = useState("");
@@ -21,27 +19,34 @@ export default function Home() {
       </div>
 
       <div className="rounded-xl bg-black">
-        <video id="video" className="rounded-xl rounded-b-none" src={video_url} autoPlay muted loop playsInline disablePictureInPicture></video>
+        <video id="video" className="rounded-xl rounded-b-none" src={'/kokona.mp4'} autoPlay muted loop playsInline disablePictureInPicture></video>
       </div>
-      <p className="mt-2 mb-2">SOCIAL</p>
-      <div className="flex flex-wrap space-x-4">
+      <p className="mt-2 mb-2">SOCIALS</p>
+      <div className="flex flex-col gap-2 items-center align-center justify-center">
         <a href="https://twitter.com/tixosu" className="btn btn-sm btn-neutral">
-          <Image id="white" className="h-4 w-4" src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg"></Image>
-          <p>TWITTER</p>
+          <Image id="white" alt="Twitter" className="h-4 w-4" width={4} height={4} src="/twitter.svg"></Image>
+          <div className="flex p-0 m-0 gap-1">
+            <p>TWITTER</p>
+            {/* <p className="text-[0.5rem] h-max align-text-bottom text-gray-500">TWITTER</p> */}
+          </div>
         </a>
         <a href="https://www.instagram.com/emm_ment/" className="btn btn-sm btn-neutral">
-          <Image id="white" className="h-4 w-4" src="https://icons.getbootstrap.com/assets/icons/instagram.svg"></Image>
+          <Image id="white" alt="Instagram" className="h-4 w-4" width={4} height={4} src="/instagram.svg"></Image>
           <p>INSTAGRAM</p>
         </a>
         <a href="https://www.linkedin.com/in/emment/" className="btn btn-sm btn-neutral">
-          <Image id="white" className="h-4 w-4" src="https://icons.getbootstrap.com/assets/icons/linkedin.svg"></Image>
+          <Image id="white" alt="LinkedIn" className="h-4 w-4" width={4} height={4} src="/linkedin.svg"></Image>
           <p>LINKEDIN</p>
         </a>
+        <a href="https://github.com/Tx-ID" className="btn btn-sm btn-neutral">
+          <Image id="white" alt="Github" className="h-4 w-4" width={4} height={4} src="/github.svg"></Image>
+          <p>GITHUB</p>
+        </a>
       </div>
-      <p className="mt-2 mb-2 uppercase">miscellaneous</p>
-      <div className="flex flex-wrap mb-4 space-x-4">
+      <p className="mt-2 mb-2 uppercase">misc</p>
+      <div className="flex flex-col gap-2 items-center align-center justify-center mb-4">
         <button onClick={() => { setAlert("Discord username has been copied to clipboard."); setIsOpen(true); navigator.clipboard.writeText("tix1"); }} className="btn btn-sm btn-neutral">
-          <Image id="white" className="h-4 w-4" src="https://icons.getbootstrap.com/assets/icons/discord.svg"></Image>
+          <Image id="white" alt="Discord" className="h-4 w-4" width={4} height={4} src="/discord.svg"></Image>
           <p>DISCORD</p>
         </button>
         <a href="https://osu.ppy.sh/users/11421465" className="btn btn-sm btn-neutral">
@@ -49,7 +54,7 @@ export default function Home() {
           <p>OSU!</p>
         </a>
         <a href="https://www.roblox.com/users/65461579/profile" className="btn btn-sm btn-neutral">
-          <Image id="white" className="h-4 w-4" src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Roblox_Logo.svg"></Image>
+          <Image id="white" alt="Roblox" className="h-4 w-4" width={4} height={4} src="/roblox.svg"></Image>
           <p>ROBLOX</p>
         </a>
       </div>
