7f02a4ffe8ddd835a7a6e5fab30ac15f840f0ef3
Tix <159746173+snhrkkn@users.noreply.github.com>
2024-06-09 14:37:13 +0700

    opengraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,16 @@ const lexend = Lexend({ subsets: ['latin'] })
 export const metadata = {
   title: `Tix's Portal`,
   description: '',
+
+  openGraph: {
+    title: `Tix's Portal`,
+    description: `Portal to my Social Media`,
+    url: 'https://tix.fyi',
+    siteName: `Tix's Portal`,
+    images: [],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({ children }) {
