1240f14e6f4449d1dd5ec2d0446705c16ae022e7
Tix <67917789+Tx-ID@users.noreply.github.com>
2024-10-09 10:31:57 +0700

    simplify

--- src/app/page.js
+++ src/app/page.js
@@ -9,55 +9,35 @@ export default function Home() {
   const [alertText, setAlert] = useState("");
 
   return (
-    <main className="bg-base-200 flex flex-col max-w-3xl rounded-xl items-center">
-      <div id="alert" className={"absolute alert max-w-xl " + (!isOpen ? "open" : "")}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <div className='flex justify-center align-middle items-center'>
+      <div className="absolute inset-0 z-0">
+        <Image alt="Background" src="/background.jpg" style={{objectFit: "cover"}} fill={true} className='opacity-5'></Image>
+      </div>
+      <div id="alert" className={"z-10 absolute alert max-w-xl " + (isOpen ? "" : "alert-closed")}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        </svg>
         <span>{alertText}</span>
         <div>
-          <button onClick={() => { setIsOpen(false) }} className="btn btn-sm btn-primary">OK</button>
+          <button onClick={() => setIsOpen(false)} className="btn btn-sm btn-primary">OK</button>
         </div>
       </div>
+      <div className="bg-base-200 flex flex-col max-w-3xl rounded-xl items-center z-10 relative">
+        <div className="rounded-xl bg-black z-10">
+          <video id="video" className="rounded-xl rounded-b-none bg-black" src={'/video.mp4'} autoPlay muted loop playsInline disablePictureInPicture></video>
+        </div>
 
-      <div className="rounded-xl bg-black">
-        <video id="video" className="rounded-xl rounded-b-none" src={'/kokona.mp4'} autoPlay muted loop playsInline disablePictureInPicture></video>
+        <div className="flex flex-wrap gap-2 items-center justify-center mt-4 mb-4 z-20">
+          <a href="https://github.com/Tx-ID" className="btn btn-sm btn-neutral">
+            <Image id="white" alt="Github" className="h-4 w-4" width={4} height={4} src="/svg/github.svg" />
+            <p>GITHUB</p>
+          </a>
+          <button onClick={() => { setAlert("Discord username has been copied to clipboard."); setIsOpen(true); navigator.clipboard.writeText("tix1"); }} className="btn btn-sm btn-neutral">
+            <Image id="white" alt="Discord" className="h-4 w-4" width={4} height={4} src="/svg/discord.svg" />
+            <p>DISCORD</p>
+          </button>
+        </div>
       </div>
-      <p className="mt-2 mb-2">SOCIALS</p>
-      <div className="flex flex-col gap-2 items-center align-center justify-center">
-        <a href="https://twitter.com/tixosu" className="btn btn-sm btn-neutral">
-          <Image id="white" alt="Twitter" className="h-4 w-4" width={4} height={4} src="/twitter.svg"></Image>
-          <div className="flex p-0 m-0 gap-1">
-            <p>TWITTER</p>
-            {/* <p className="text-[0.5rem] h-max align-text-bottom text-gray-500">TWITTER</p> */}
-          </div>
-        </a>
-        <a href="https://www.instagram.com/emm_ment/" className="btn btn-sm btn-neutral">
-          <Image id="white" alt="Instagram" className="h-4 w-4" width={4} height={4} src="/instagram.svg"></Image>
-          <p>INSTAGRAM</p>
-        </a>
-        <a href="https://www.linkedin.com/in/emment/" className="btn btn-sm btn-neutral">
-          <Image id="white" alt="LinkedIn" className="h-4 w-4" width={4} height={4} src="/linkedin.svg"></Image>
-          <p>LINKEDIN</p>
-        </a>
-        <a href="https://github.com/Tx-ID" className="btn btn-sm btn-neutral">
-          <Image id="white" alt="Github" className="h-4 w-4" width={4} height={4} src="/github.svg"></Image>
-          <p>GITHUB</p>
-        </a>
-      </div>
-      <p className="mt-2 mb-2 uppercase">misc</p>
-      <div className="flex flex-col gap-2 items-center align-center justify-center mb-4">
-        <button onClick={() => { setAlert("Discord username has been copied to clipboard."); setIsOpen(true); navigator.clipboard.writeText("tix1"); }} className="btn btn-sm btn-neutral">
-          <Image id="white" alt="Discord" className="h-4 w-4" width={4} height={4} src="/discord.svg"></Image>
-          <p>DISCORD</p>
-        </button>
-        <a href="https://osu.ppy.sh/users/11421465" className="btn btn-sm btn-neutral">
-          <div className="fill-white h-4 w-4"><OsuIcon /></div>
-          <p>OSU!</p>
-        </a>
-        <a href="https://www.roblox.com/users/65461579/profile" className="btn btn-sm btn-neutral">
-          <Image id="white" alt="Roblox" className="h-4 w-4" width={4} height={4} src="/roblox.svg"></Image>
-          <p>ROBLOX</p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
